1f5d738d621c94deac9d5666317f25a977f36f82
Mathis <mathun3003@gmail.com>
2025-05-31 19:39:54 +0200

    YOLO11n saddle training jobs

--- model-training/model_training/core/schemas.py
+++ model-training/model_training/core/schemas.py
@@ -16,11 +16,31 @@ class YoloTrainArgs(BaseModel):
         extra = "allow"
 
 
+class DataConfigSchema(BaseModel):
+    path: str = Field(..., description="Absolute path to dataset directory containing images/ and labels/ directories")
+    train: str = Field(..., description="Path to training directory or .txt file, relative to 'path' argument")
+    val: str = Field(..., description="Path to validation directory or .txt file, relative to 'path' argument")
+    test: Optional[str] = Field(..., description="Path to test directory or .txt file, relative to 'path' argument")
+    nc: int = Field(..., description="Number of classes contained in the dataset")
+    names: dict[int, str] = Field(..., description="Mapping from class ID to class name")
+
+
+class DataSplitArgs(BaseModel):
+    weights: tuple[float, float, float] = Field(
+        ..., description="Ratios for dataset splitting (train, val, test)"
+    )
+    annotated_only: bool = Field(..., description="Whether to split annotated images only")
+
+    class Config:
+        extra = "forbid"
+
+
 class TrainConfigSchema(BaseModel):
     project_name: str = Field(..., description="Name of the W&B project")
     output_dir: str | PathLike = Field(..., description="Directory to save model runs")
     model: str | PathLike = Field(..., description="YOLO model name or path to weights")
     train_args: YoloTrainArgs = Field(..., description="Training arguments for Ultralytics YOLO class")
+    data_split_args: Optional[DataSplitArgs] = Field(None, description="Data split arguments for Ultralytics YOLO class")
 
     class Config:
         extra = "forbid"
--- model-training/model_training/trainer.py
+++ model-training/model_training/trainer.py
@@ -8,10 +8,11 @@ import wandb
 import yaml
 from pydantic import ValidationError
 from ultralytics import YOLO
+from ultralytics.data.split import autosplit
 # isort: on
 
 from model_training.core.constants import WANDB_PROJECT
-from model_training.core.schemas import TrainConfigSchema
+from model_training.core.schemas import DataConfigSchema, DataSplitArgs, TrainConfigSchema
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -31,24 +32,50 @@ class Trainer:
             raise ValueError("Invalid datatype for config_path, expected Path or str")
 
         self.config_path = config_path
-        self.config: TrainConfigSchema = self._load_config()
+        self.run_config: TrainConfigSchema = self._load_run_config()
+        self.data_config = self._load_data_config()
         self.model, self.model_name = self._load_model()
         self.run_name = self._generate_run_name()
-        self.output_dir = Path(self.config.output_dir) / self.run_name
+        self.output_dir = Path(self.run_config.output_dir) / self.run_name
 
         self._init_wandb()
+        if self.run_config.data_split_args:
+            self._split_dataset(split_args=self.run_config.data_split_args, data_path=self.data_config.path)
 
-    def _load_config(self) -> TrainConfigSchema:
+    @staticmethod
+    def _split_dataset(split_args: DataSplitArgs, data_path: str) -> None:
+        """
+        Performs an auto-split of the dataset based on the split ratios
+        :param split_args: Split arguments
+        :param data_path: Absolute path to images directory
+        :return: Creates autosplit_train.txt, autosplit_val.txt, autosplit_test.txt
+        """
+        autosplit(path=(Path(data_path) / 'images').as_posix(), **split_args.model_dump())
+
+    def _load_run_config(self) -> TrainConfigSchema:
         """Loads and validates YAML config file"""
         with open(self.config_path, "r") as f:
             raw_config = yaml.safe_load(f)
         try:
             return TrainConfigSchema(**raw_config)
         except ValidationError as e:
-            logger.error("❌ Config validation error:\n%s", e)
+            logger.error("❌ Run config validation error:\n%s", e)
             raise SystemExit(1)
         except ValueError as e:
-            logger.error("Invalid argument configuration: \n%s", e)
+            logger.error("Invalid argument for run configuration: \n%s", e)
+            raise SystemExit(1)
+
+    def _load_data_config(self) -> DataConfigSchema:
+        """Loads and validates YAML config file for a YOLO dataset."""
+        with open(self.run_config.train_args.data, "r") as f:
+            raw_config = yaml.safe_load(f)
+        try:
+            return DataConfigSchema(**raw_config)
+        except ValidationError as e:
+            logger.error("❌ Data config validation error:\n%s", e)
+            raise SystemExit(1)
+        except ValueError as e:
+            logger.error("Invalid argument for data configuration: \n%s", e)
             raise SystemExit(1)
 
     def _generate_run_name(self) -> str:
@@ -57,7 +84,7 @@ class Trainer:
         :return: Run name
         """
         timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-        return f"{self.config.project_name}_{self.model_name}_{timestamp}"
+        return f"{self.run_config.project_name}_{self.model_name}_{timestamp}"
 
     @staticmethod
     def _init_wandb() -> None:
@@ -69,7 +96,7 @@ class Trainer:
         Loads Ultralytics YOLO model from YAML config
         :return: Ultralytics YOLO model and corresponding model name
         """
-        model_path = Path(self.config.model)
+        model_path = Path(self.run_config.model)
         model = YOLO(model_path.as_posix())
         return model, model_path.stem
 
@@ -78,7 +105,7 @@ class Trainer:
         Invokes Ultralytics training job
         :return: None
         """
-        train_args = self.config.train_args.model_dump()
+        train_args = self.run_config.train_args.model_dump()
         train_args.update(
             {
                 "project": WANDB_PROJECT,
