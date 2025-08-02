a57895e64bbea04d0b9114b0757d49d9e16106f2
Mathis <mathun3003@gmail.com>
2025-08-02 11:46:26 +0200

    first attempt Arduino-ESP32 integration

--- model-deployment/yolo11_detect/main/main.cpp
+++ model-deployment/yolo11_detect/main/main.cpp
@@ -3,6 +3,8 @@
 #include "camera_capture.hpp"
 #include "esp_camera.h"
 #include "sd_handling.h"
+#include "Arduino.h"
+
 
 const char *TAG = "yolo_main";
 
@@ -13,9 +15,33 @@ static size_t log_psram(const char *label)
     return free_psram;
 }
 
+
 extern "C" void app_main(void)
 {
     const float confidence_threshold = 0.1;
+
+    // Init Arduino
+    initArduino();
+
+    Serial.begin(115200);
+    while(!Serial){
+        delay(5000);  // Give time to switch to the serial monitor
+        Serial.println(F("\nSetup ... "));
+
+        Serial.println(F("Initialise the radio"));
+        int16_t state = radio.begin();
+        debug(state != RADIOLIB_ERR_NONE, F("Initialise radio failed"), state, true);
+
+        // Setup the OTAA session information
+        state = node.beginOTAA(joinEUI, devEUI, appKey, appKey);
+        debug(state != RADIOLIB_ERR_NONE, F("Initialise node failed"), state, true);
+
+        Serial.println(F("Join ('login') the LoRaWAN Network"));
+        state = node.activateOTAA();
+        debug(state != RADIOLIB_LORAWAN_NEW_SESSION, F("Join failed"), state, true);
+
+        Serial.println(F("Ready!\n"));
+    }
 
     // Initialize SD card
     esp_err_t sd_ret = init_sd_card();
@@ -76,7 +102,7 @@ extern "C" void app_main(void)
         ESP_LOGI(TAG, "-> Found %d bikes and %d saddles in this frame.", bike_count, saddle_count);
         
         // Save image to SD
-        if (is_sd_card_mounted() && (bike_count > 0 || saddle_count > 0)) {
+        if (is_sd_card_mounted()) {
             ESP_LOGI(TAG, "Saving image and detection results to SD card...");
             
             char image_filename[64];
@@ -93,17 +119,57 @@ extern "C" void app_main(void)
                 ESP_LOGE(TAG, "Failed to save image");
             }
             
-            esp_err_t results_ret = save_detection_results(detect_results, confidence_threshold, results_filename);
-            if (results_ret == ESP_OK) {
-                ESP_LOGI(TAG, "Detection results saved successfully");
+            if (bike_count > 0) {
+                esp_err_t results_ret = save_detection_results(detect_results, confidence_threshold, results_filename);
+                if (results_ret == ESP_OK) {
+                    ESP_LOGI(TAG, "Detection results saved successfully");
+                } else {
+                    ESP_LOGE(TAG, "Failed to save detection results");
+                }
             } else {
-                ESP_LOGE(TAG, "Failed to save detection results");
+                ESP_LOGI(TAG, "No bikes detected, skipping results save");
             }
             
             increment_image_counter();
         } else if (is_sd_card_mounted()) {
             ESP_LOGI(TAG, "No objects detected, skipping SD card save");
         }
+
+        /*
+        -------------------------------------------------------------------
+        Send data over LoRaWAN
+        -------------------------------------------------------------------
+        */
+
+        // This is the place to gather the sensor inputs
+        // Instead of reading any real sensor, we just generate some random numbers as example
+        uint8_t value1 = radio.random(100);
+        uint16_t value2 = radio.random(2000);
+
+        // Build payload byte array
+        uint8_t uplinkPayload[3];
+        uplinkPayload[0] = value1;
+        uplinkPayload[1] = highByte(value2);   // See notes for high/lowByte functions
+        uplinkPayload[2] = lowByte(value2);
+        
+        // Perform an uplink
+        int16_t state = node.sendReceive(uplinkPayload, sizeof(uplinkPayload));    
+        debug(state < RADIOLIB_ERR_NONE, F("Error in sendReceive"), state, false);
+
+        // Check if a downlink was received 
+        // (state 0 = no downlink, state 1/2 = downlink in window Rx1/Rx2)
+        if(state > 0) {
+            Serial.println(F("Received a downlink"));
+        } else {
+            Serial.println(F("No downlink received"));
+        }
+
+        Serial.print(F("Next uplink in "));
+        Serial.print(uplinkIntervalSeconds);
+        Serial.println(F(" seconds\n"));
+        
+        // Wait until next uplink - observing legal & TTN FUP constraints
+        delay(uplinkIntervalSeconds * 1000UL);  // delay needs milli-seconds
 
         // Only return the frame buffer after all operations are done to avoid issues with accessing the frame buffer after it has been returned
         esp_camera_fb_return(fb);
